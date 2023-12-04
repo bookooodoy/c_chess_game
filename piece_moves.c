f5d77e248ed92fc7989befb6f78ef2d423fa0639
nraymond <nraymond@nathan>
2023-12-04 17:33:42 +0100

    added rook legal moves

--- piece_moves.c
+++ piece_moves.c
@@ -3,19 +3,20 @@
 int	check_square_valid(board ChessBoard, int y, int x, unsigned int team)
 {
 	// TODO: add check function to verify if we are not in check and if so the move is valid
-	if ((ChessBoard.ChessSquare[y][x] && ChessBoard.ChessSquare[y][x].isEmpty == 1) || (ChessBoard.ChessSquare[y][x] && ChessBoard.ChessSquare[y][x].ChessPiece.color != team && ChessBoard.ChessSquare[y][x].ChessPiece.name != 'k' && ChessBoard.ChessSquare[y][x].ChessPiece.name != 'K'))
+	if (ChessBoard.ChessSquare[y][x] && ChessBoard.ChessSquare[y][x].isEmpty == 1)
 		return (1);
+	if (ChessBoard.ChessSquare[y][x] && ChessBoard.ChessSquare[y][x].ChessPiece.color != team && ChessBoard.ChessSquare[y][x].ChessPiece.name != 'k' && ChessBoard.ChessSquare[y][x].ChessPiece.name != 'K')
+		return (2);
 	return (0);
 }
 
 int	**get_legal_moves_bpawn(piece piece, board ChessBoard)
 {
 	int	**legal_moves;
-	int	coordinates[2]
-	int	i;
+	int	coordinates[2];
+	int	i = 0;
 
-	i = 0;
-	legal_moves = (int **)malloc(sizeof(int) * 6);
+	legal_moves = (int **)malloc(sizeof(int *) * 6);
 	if (!legal_moves)
 		return (NULL);
 	if (check_square_valid(ChessBoard, piece.y_cords + 1, piece.x_cords, piece.color))
@@ -53,11 +54,10 @@ int	**get_legal_moves_bpawn(piece piece, board ChessBoard)
 int	**get_legal_moves_wpawn(piece piece, board ChessBoard)
 {
 	int	**legal_moves;
-	int	coordinates[2]
-	int	i;
+	int	coordinates[2];
+	int	i = 0;
 
-	i = 0;
-	legal_moves = (int **)malloc(sizeof(int) * 6);
+	legal_moves = (int **)malloc(sizeof(int *) * 6);
 	if (!legal_moves)
 		return (NULL);
 	if (check_square_valid(ChessBoard, piece.y_cords - 1, piece.x_cords, piece.color))
@@ -93,11 +93,10 @@ int	**get_legal_moves_wpawn(piece piece, board ChessBoard)
 int	**get_legal_moves_knight(piece piece, board ChessBoard)
 {
 	int	**legal_moves;
-	int	coordinates[2]
-	int	i;
+	int	coordinates[2];
+	int	i = 0;
 
-	i = 0;
-	legal_moves = (int **)malloc(sizeof(int) * 9);
+	legal_moves = (int **)malloc(sizeof(int *) * 9);
 	if (!legal_moves)
 		return (NULL);
 	if (check_square_valid(ChessBoard, piece.y_cords + 2, piece.x_cords + 1, piece.color))
@@ -152,6 +151,98 @@ int	**get_legal_moves_knight(piece piece, board ChessBoard)
 	return (legal_moves);
 }
 
+int	**get_legal_moves_rook(piece piece, board ChessBoard)
+{
+	int	**legal_moves;
+	int	coordinates[2]
+	int	i;
+	int	j;
+	int	ret;
+
+	j = 0;
+	legal_moves = (int **)malloc(sizeof(int *) * 15);
+	if (!legal_moves)
+		return (NULL);
+	while (1)
+	{
+		i = 1;
+		while (ret = check_valid(ChessBoard, piece.y_cords - i, piece.x_cords, piece.color)) 
+		{
+			if (ret == 2)
+			{
+				coordinates[0] = piece.y_cords - i;
+				coordinates[1] = piece.x_cords;
+				legal_moves[j++] = coordinates;
+				break ;
+			}
+			else if (ret == 1)
+			{
+				coordinates[0] = piece.y_cords - i;
+				coordinates[1] = piece.x_cords;
+				legal_moves[j++] = coordinates;
+			}
+			i++;
+		}
+		i = 1;
+		while (ret = check_valid(ChessBoard, piece.y_cords + i, piece.x_cords, piece.color)) 
+		{
+			if (ret == 2)
+			{
+				coordinates[0] = piece.y_cords + i;
+				coordinates[1] = piece.x_cords;
+				legal_moves[j++] = coordinates;
+				break ;
+			}
+			else if (ret == 1)
+			{
+				coordinates[0] = piece.y_cords + i;
+				coordinates[1] = piece.x_cords;
+				legal_moves[j++] = coordinates;
+			}
+			i++;
+		}
+		i = 1;
+		while (ret = check_valid(ChessBoard, piece.y_cords, piece.x_cords - i, piece.color)) 
+		{
+			if (ret == 2)
+			{
+				coordinates[0] = piece.y_cords;
+				coordinates[1] = piece.x_cords - i;
+				legal_moves[j++] = coordinates;
+				break ;
+			}
+			else if (ret == 1)
+			{
+				coordinates[0] = piece.y_cords;
+				coordinates[1] = piece.x_cords - i;
+				legal_moves[j++] = coordinates;
+			}
+			i++;
+		}
+		i = 1;
+		while (ret = check_valid(ChessBoard, piece.y_cords, piece.x_cords + i, piece.color)) 
+		{
+			if (ret == 2)
+			{
+				coordinates[0] = piece.y_cords;
+				coordinates[1] = piece.x_cords + i;
+				legal_moves[j++] = coordinates;
+				break ;
+			}
+			else if (ret == 1)
+			{
+				coordinates[0] = piece.y_cords;
+				coordinates[1] = piece.x_cords + i;
+				legal_moves[j++] = coordinates;
+			}
+			i++;
+		}
+		break ;
+	}
+	legal_moves[j] = NULL;
+	return (legal_moves);
+}
+
 int	**get_legal_moves_king(piece piece, board ChessBoard, int **threatmap)
 {
 	int	**legal_moves;
@@ -159,7 +250,7 @@ int	**get_legal_moves_king(piece piece, board ChessBoard, int **threatmap)
 	int	i;
 
 	i = 0;
-	legal_moves = (int **)malloc(sizeof(int) * 5);
+	legal_moves = (int **)malloc(sizeof(int *) * 5);
 	if (!legal_moves)
 		return (NULL);
 		
