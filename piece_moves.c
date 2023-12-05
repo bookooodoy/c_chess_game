6cea33ce922a7ee0371c2ef504374e194cebe038
nraymond <nraymond@nathan>
2023-12-05 05:06:22 +0100

    bug in legal moves, bad coords and repeats the first cord he finds

--- piece_moves.c
+++ piece_moves.c
@@ -494,40 +494,42 @@ int	**get_legal_moves_king(piece piece, board ChessBoard, int ***threatmap)
 
 int	main(void)
 {
-	char *fenstring = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+	char *fenstring = "2B1R3/4q3/1K1P3n/P1N2p1P/2k2B2/2p1p3/1P4p1/8";
 	board ChessBoard = Fill_starting_position(fenstring);
 	
 	ptr_tab(ChessBoard);
-         // update the board
-	 ChessBoard.ChessSquare[1][0].isEmpty = 1;
-	 ChessBoard.ChessSquare[3][0].isEmpty = 0;
-         ChessBoard.ChessSquare[3][0].ChessPiece.name = 'p';
-         ChessBoard.ChessSquare[3][0].ChessPiece.y_cords = 3;
-         ChessBoard.ChessSquare[3][0].ChessPiece.x_cords = 0;
+        /*// update the board
+	ChessBoard.ChessSquare[1][0].isEmpty = 1;
+        ChessBoard.ChessSquare[3][0].ChessPiece.name = 'p';
+        ChessBoard.ChessSquare[3][0].ChessPiece.y_cords = 3;
+        ChessBoard.ChessSquare[3][0].ChessPiece.x_cords = 0;
+	ChessBoard.ChessSquare[3][0].ChessPiece.piece_fullmove += 1;
 
-         // get the new fenstring
-         char *newfenstring = update_fenstring(ChessBoard);
-         printf("\n\n\n SECOND BOARD AFTER CHANGE \n\n\n");
-	 ptr_tab(ChessBoard);
-         printf("\n%s\n", newfenstring);
+         // get the new fenstring*/
 	 //int	**legal_king_moves;
 	 //int	***threatmap = get_threatmap(ChessBoard, 0);
 	 //legal_king_moves = get_legal_moves_king(ChessBoard.ChessSquare[0][4].ChessPiece, ChessBoard, threatmap);
-	 int i = 0;
-	 int k = 0;
-	 int	**legal_pawn_move;
+	int i = 0;
+	int k = 0;
+	int j;
+	int	***threatmap;
 
-	 legal_pawn_move = get_legal_moves_bpawn(ChessBoard.ChessSquare[3][0].ChessPiece, ChessBoard); 
-	 while (i < 2)
-	 {
-		 k = 0;
-		 while (k < 2)
-		 {
-			 printf("%d, ", legal_pawn_move[i][k]);
-			 k++;
-		 }
-		 printf("\n");
-		 i++;
-	 }
-         return 0;
+	threatmap = get_threatmap(ChessBoard, 1); 
+	while (threatmap[i])
+	{
+		while (threatmap[i][k])
+		{
+			j = 0;
+			while (j < 2)
+			{
+				if (threatmap[i][k])
+					printf("[%i,", threatmap[i][k][j]);
+				j++;
+			}
+			printf("\n");
+			k++;
+		}
+		i++;
+	}
+	return 0;
 }
